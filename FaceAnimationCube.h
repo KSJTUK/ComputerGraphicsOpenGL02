05e2e566fe512df829dd4d3cb5b56a5f5378c14c
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-24 21:11:38 +0900

    면마다 다른 애니메이션을 출력할 클래스 기본 틀 작성

--- FaceAnimationCube.h
+++ FaceAnimationCube.h
@@ -1,5 +1,28 @@
 #pragma once
+#include "vertex_info.h"
+#include "gl_headers.h"
+#include "GraphicBuffers.h"
+#include <vector>
+#include <memory>
+
+class CubeFace {
+	std::vector<Vertex> m_vertex{ };
+	std::vector<unsigned int> m_index{ };
+
+	glm::vec3 m_position{ };
+	glm::vec3 m_scale{ };
+	glm::vec3 m_rotate{ };
+};
 
 class FaceAnimationCube {
+public:
+	FaceAnimationCube() { }
+	~FaceAnimationCube() { }
 
+private:
+	std::vector<Vertex> m_faces{ };
+	std::vector<unsigned int> m_faceIndex{ };
+
+public:
+	void Update();
 };
